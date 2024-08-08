58b6d567900893181db54ef53c08ec8a2b7d8138
iamjoker021 <iamjoker021@gmail.com>
2024-08-08 09:22:43 +0530

    feat: Add HTML header-main-footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,24 @@
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <header>
+        <ul>
+          <li><h1>Conductor App</h1></li>
+          <li>Profile Photo</li>
+        </ul>
+        <nav>
+          <ul>
+            <li>Place Ticket</li>
+            <li>View Ticket</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <p>Main Content Goes Here</p>
+      </main>
+      <footer>
+        Made with &#10084; by <a href="https://github.com/iamjoker021/CONDUCTOR_APP_FRONTEND">Joker</a>
+      </footer>
     </>
   )
 }
