ed0e3a0e24890311a394d72c854b07c8fc475137
iamjoker021 <iamjoker021@gmail.com>
2024-08-09 15:55:22 +0530

    feat: Open link in new tab on Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            Made with &#10084; by <a href="https://github.com/iamjoker021/CONDUCTOR_APP_FRONTEND">Joker</a>
+            Made with &#10084; by <a href="https://github.com/iamjoker021/CONDUCTOR_APP_FRONTEND" target="_blank" rel="noopener noreferrer">Joker</a>
         </footer>
     )
 }
