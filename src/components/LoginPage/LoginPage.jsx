54ca714ed9e90b99006a1d552a102dd1df68b38b
iamjoker021 <iamjoker021@gmail.com>
2024-08-09 11:48:17 +0530

    feat: Add Login functionality and create helper useLogin hook

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,17 +1,45 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useLogin } from "../../hooks/useLogin";
 
 const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const { login, isLoading, error } = useLogin()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await login(email, password);
+    }
+
     return (
         <>
         <h2>Login Form</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="email">Email: </label>
-            <input type="email" name="email" id="email" required />
+            <input 
+                type="email" 
+                name="email" 
+                id="email" 
+                value={email} 
+                onChange={e => setEmail(e.target.value)} 
+                required 
+            />
             <label htmlFor="password">Password: </label>
-            <input type="password" name="password" id="password" required />
-            <button type="submit">Log In</button>
+            <input 
+                type="password" 
+                name="password" 
+                id="password" 
+                value={password} 
+                onChange={e => setPassword(e.target.value)} 
+                required 
+            />
+            {<button type="submit" disabled={isLoading}>Log In</button>}
+            {isLoading && <p>Logging In User ....</p>}
+            {error !==null && <p>{error}</p>}
         </form>
-        <p>Didn't have acount? <Link to="/auth/signin">Create Account</Link></p>
+        <p>Didn't have account? <Link to="/auth/signin">Create Account</Link></p>
         </>
     )
 }
---
+++ src/hooks/useLogin.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogin = () => {
+    const { dispatch } = useAuthContext();
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const navigate = useNavigate();
+
+    const login = async (email, password) => {
+        setIsLoading(true);
+        setError(null);
+
+        try {
+            const SERVER_URL = "http://localhost:3000";
+            const USER_LOGIN = "/api/auth/login";
+            const jsonBody = JSON.stringify({ email, password });
+            const response = await fetch(SERVER_URL+USER_LOGIN, { method: "POST", headers: { 'Content-type': 'application/json' }, body: jsonBody })
+            const data = await response.json();
+            console.log('Log In Log: ',data);
+            
+            if (!response.ok) {
+                setError(data.msg);
+            }
+            else {
+                localStorage.setItem('user', JSON.stringify(data));
+                dispatch({type: 'LOGIN', payload: data});
+                navigate('/');
+            }
+        } 
+        catch (error) {
+            console.log(error);
+            setError(JSON.stringify(error));
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+
+    return { login, isLoading, error };
+}
