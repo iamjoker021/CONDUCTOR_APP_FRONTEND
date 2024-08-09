b91413f69ebfd8564563120f837688f1535a797c
iamjoker021 <iamjoker021@gmail.com>
2024-08-09 15:48:43 +0530

    style: Add toogle navbar effect on ui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [currentLink, setCurrentLink] = useState('place');
+  const handleClick = (e) => {
+    const classContains = (value) => Array(...e.target.classList).includes(value);
+    if (classContains('view')) {
+      setCurrentLink('view');
+    }
+    else if (classContains('place')) {
+      setCurrentLink('place');
+    }
+    e.preventDefault();
+  }
+
   return (
-      <nav>
-        <ul>
-          <li><Link to="/place-ticket">Place Ticket</Link></li>
-          <li><Link to="/tickets">View Tickets</Link></li>
-        </ul>
+      <nav onClick={handleClick} className="navbar">
+          <Link className={'nav-item place' + (currentLink === 'place' ? ' active' : '')} to="/place-ticket">Place Ticket</Link>
+          <Link className={'nav-item view' + (currentLink === 'view' ? ' active' : '')} to="/tickets">View Tickets</Link>
       </nav>
   )
 }
