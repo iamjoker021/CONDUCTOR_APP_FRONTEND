574068b336e3d77dc654285bdedf3c7bcf30fadf
iamjoker021 <iamjoker021@gmail.com>
2024-08-09 11:56:11 +0530

    feat: Add Logout functionality with useLogout hook

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
+import { useLogout } from "../../hooks/useLogout";
+
 const Navbar = () => {
+  
+  const { logout } = useLogout();
+    const handleClick = () => {
+      logout()
+    }
+
     return (
         <nav>
           <ul>
             <li>Place Ticket</li>
             <li>View Ticket</li>
+            <li><button type="button" onClick={handleClick}>Log Out</button></li>
           </ul>
         </nav>
     )
---
+++ src/hooks/useLogout.js
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogout = () => {
+    const { dispatch } = useAuthContext();
+
+    const navigate = useNavigate();
+
+    const logout = () => {
+        localStorage.removeItem('user');
+        dispatch({type: 'LOGOUT'});
+        navigate('/auth');
+    }
+
+    return { logout };
+}
