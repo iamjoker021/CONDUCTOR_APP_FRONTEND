eb131f23ddd30f6ff1f2c98ea0b3c2b869795b89
iamjoker021 <iamjoker021@gmail.com>
2024-08-13 10:31:22 +0530

    fix: Earlier we change Nav style based on click. Now, On navigation the Navbar changes correctly by using useLocation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  const [currentLink, setCurrentLink] = useState('place');
-  const handleClick = (e) => {
-    const classContains = (value) => Array(...e.target.classList).includes(value);
-    if (classContains('view')) {
-      setCurrentLink('view');
-    }
-    else if (classContains('place')) {
-      setCurrentLink('place');
-    }
-    e.preventDefault();
+  const location = useLocation();
+  let currentLink;
+  switch (location.pathname) {
+    case '/place-ticket':
+      currentLink = 'place';
+      break;
+    case '/tickets':
+      currentLink = 'view';
+      break;
+    default:
+      break;
   }
 
   return (
-      <nav onClick={handleClick} className="navbar">
+      <nav className="navbar">
           <Link className={'nav-item place' + (currentLink === 'place' ? ' active' : '')} to="/place-ticket">Place Ticket</Link>
           <Link className={'nav-item view' + (currentLink === 'view' ? ' active' : '')} to="/tickets">View Tickets</Link>
       </nav>
