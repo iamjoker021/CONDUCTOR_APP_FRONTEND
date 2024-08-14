6d2add7337a3ee27a1aaeacc08e8913ddcf2df11
iamjoker021 <iamjoker021@gmail.com>
2024-08-14 13:33:55 +0530

    chore: Remove non-used library

--- src/components/SignInPage.jsx
+++ src/components/SignInPage.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import { useSignin } from "../hooks/useSignin";
 
 const SignInPage = () => {
