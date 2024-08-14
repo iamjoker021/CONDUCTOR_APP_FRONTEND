57700f8d863b4b946f4dbf4caf6cca4f6309c0d0
iamjoker021 <iamjoker021@gmail.com>
2024-08-14 15:19:00 +0530

    doc: Add ticket status and style them

--- src/components/TicketCard.jsx
+++ src/components/TicketCard.jsx
@@ -9,6 +9,7 @@ const TicketCard = ({ticket}) => {
             <div className="qrinfo">
                 <p>Issue Time: {new Date(ticket.issue_time).toLocaleString()}</p>
                 <p>Expiry Time: {new Date(ticket.expiry_time).toLocaleString()}</p>
+                <p>Status: <span className={'ticket-status ' + (new Date() >= new Date(ticket.expiry_time) ? 'expired' : 'valid')}>{new Date() >= new Date(ticket.expiry_time) ? 'Expired' : 'Valid'}</span></p>
                 <Link to={ticket.ticket_unique_identifier} state={ticket}>Click here More Info</Link>
             </div>
         </div>
