fcc94cee677c3d34815ff2db9685d0690f4af41d
iamjoker021 <iamjoker021@gmail.com>
2024-08-18 13:41:26 +0530

    feat: Modify Navbar using conditional rendering for Passenger and Conductor Roles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import { Link, useLocation } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
+  const { user } = useAuthContext();
   const location = useLocation();
   let currentLink;
 
-  if (location.pathname.includes('/place-ticket')){
+  if (location.pathname.includes('/place-ticket') || location.pathname.includes('/scan')){
     currentLink = 'place';
   }
   else if (location.pathname.includes('/tickets')){
@@ -13,8 +15,17 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <Link className={'nav-item place' + (currentLink === 'place' ? ' active' : '')} to={"/passenger/place-ticket"} >Place Ticket</Link>
-      <Link className={'nav-item view' + (currentLink === 'view' ? ' active' : '')} to={"/passenger/tickets"} >View Tickets</Link>
+      {user.role === 'passenger' ?
+      <>
+      <Link className={'nav-item place' + (currentLink === 'place' ? ' active' : '')} to={`/${user.role}/place-ticket`} >Place Ticket</Link>
+      <Link className={'nav-item view' + (currentLink === 'view' ? ' active' : '')} to={`/${user.role}/tickets`} >View Tickets</Link>
+      </>
+      :
+      <>
+      <Link className={'nav-item place' + (currentLink === 'place' ? ' active' : '')} to={`/${user.role}/ticket/scan`} >Scan Ticket</Link>
+      <Link className={'nav-item view' + (currentLink === 'view' ? ' active' : '')} to={`/${user.role}/tickets/123`} >View Tickets</Link>
+      </>
+      }
     </nav>
   )
 }
